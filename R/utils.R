73b1e36fb45598e2fad93dbce79011f9b4ca16b2
Allison Codi <allison.codi@emory.edu>
2025-03-17 13:16:40 -0400

    update subplots fn

--- R/utils.R
+++ R/utils.R
@@ -861,83 +861,131 @@ plot_cis <- function(results,
 
 #' Function to make confidence interval plots of results with multiple subplots
 #' 
-#' @param results `aggcomp_res` object or list of `aggcomp_res` objects
+#' @param results list of `aggcomp_res` objects or list of `agaipw_res` objects
 #' @param abx_labels label or list of labels to use for antibiotics variable (same length as results list)
 #' @param inf_labels label or list of labels to use for infection variable (same length as results list)
 #' @param outcome_labels label or list of labels to use for outcome variable (same length as results list)
+#' @param study_labels label or list of labels to use indicating which study result came from (same length as results list)
 #' 
 #' @import ggplot2
 #' 
-#' @return list of confidence interval ggplots
+#' @return ggplot with subplots for each results object
 plot_cis_subplots <- function(results, 
                      abx_labels = NULL,
                      inf_labels = NULL, 
-                     outcome_labels = NULL) {
+                     outcome_labels = NULL,
+                     study_labels = NULL) {
   
   # if single object, put into list for lapply
   if (!is(results, "list")) {
     results <- list(results)
   }
   
-  # Determine the global x-axis limits
-  x_lims <- lapply(results, function(res) {
-    results_vec <- res$results
-    range(c(results_vec['lower_ci_abx'], results_vec['upper_ci_abx'],
-            results_vec['lower_ci_no_abx'], results_vec['upper_ci_no_abx']))
-  })
-  global_x_min <- min(sapply(x_lims, `[`, 1))
-  global_x_max <- max(sapply(x_lims, `[`, 2))
-  
-  # Add padding for annotations
-  annotation_padding <- (global_x_max - global_x_min) * 0.5
-  global_x_max <- global_x_max + annotation_padding
-  
-  # Generate individual plots
-  plot_results <- lapply(1:length(results), function(i) {
+  # Generate individual plot data
+  plot_data <- lapply(1:length(results), function(i) {
     res <- results[[i]]
     
-    if (is.null(abx_labels)) {
+    # Get labels for plot if not passed in
+    if(is.null(abx_labels)){
       abx_label <- res$parameters$abx_var_name
     } else {
       abx_label <- abx_labels[[i]]
     }
     
-    if (is.null(inf_labels)) {
+    if(is.null(inf_labels)) {
       inf_label <- res$parameters$infection_var_name
-    } else {
+      # if no inf_var_name, use case_var_name (case control)
+      if(is.null(inf_label)){
+        inf_label <- res$parameters$case_var_name
+      }
+    } else{
       inf_label <- inf_labels[[i]]
     }
     
-    if (is.null(outcome_labels)) { 
+    if(is.null(outcome_labels)) { 
       outcome_label <- res$parameters$laz_var_name
     } else {
       outcome_label <- outcome_labels[[i]]
     }
     
+    if(is.null(study_labels)){
+      study_label <- i
+    } else{
+      study_label <- study_labels[[i]]
+    }
+    
     plot_label <- paste0("Antibiotics: ", abx_label, ", Infection: ", inf_label) 
+    plot_data <- data.frame()
     
-    # Create dataframe for plotting
-    results_vec <- res$results
-    plot_data <- data.frame(
-      subgroup = c("Antibiotics", "No Antibiotics"),
-      pt_est = c(results_vec['effect_inf_abx'], results_vec['effect_inf_no_abx']),
-      lower_ci = c(results_vec['lower_ci_abx'], results_vec['lower_ci_no_abx']),
-      upper_ci = c(results_vec['upper_ci_abx'], results_vec['upper_ci_no_abx']),
-      label = plot_label
-    )
+    # make dataframe for plotting
+    if(class(res) == "agaipw_res"){
+      # Plot AIPW 
+      res_df <- res$aipw_est$results_object$results_df
+      se_vec <- res$aipw_est$results_object$se
+      
+      abx_levels <- res_df$abx_levels
+      
+      for(i in 1:length(abx_levels)){
+        abx_level <- abx_levels[i]
+        tmp <- data.frame(subgroup = abx_level,
+                          pt_est = res_df$effect_inf_abx_level[res_df$abx_levels == abx_level],
+                          lower_ci = res_df$effect_inf_abx_level[res_df$abx_levels == abx_level] - 1.96*se_vec[paste0("effect_", abx_level)],
+                          upper_ci = res_df$effect_inf_abx_level[res_df$abx_levels == abx_level] + 1.96*se_vec[paste0("effect_", abx_level)],
+                          plot_label = plot_label,
+                          study_label = study_label)
+        
+        plot_data <- rbind(plot_data, tmp)
+      }
+      
+    } else{
+      # Plot GComp
+      res_df <- res$results$pt_est
+      se_vec <- res$results$bootstrap_results
+      
+      abx_levels <- res_df$abx_levels
+      
+      for(i in 1:length(abx_levels)){
+        abx_level <- abx_levels[i]
+        tmp <- data.frame(subgroup = abx_level,
+                          pt_est = res_df$effect_inf_abx_level[res_df$abx_levels == abx_level],
+                          lower_ci = se_vec[[paste0('Abx = ', abx_level)]]$lower_ci_effect_inf_abx_level,
+                          upper_ci = se_vec[[paste0('Abx = ', abx_level)]]$upper_ci_effect_inf_abx_level,
+                          plot_label = plot_label,
+                          study_label = study_label)
+        plot_data <- rbind(plot_data, tmp)
+      }
+      
+    }
     
-    x_loc <- global_x_max - annotation_padding / 2
+    return(plot_data)
+  })
+  
+  plot_data <- do.call(rbind, plot_data)
+  
+  # Get xaxis scaling 
+  global_x_min <- min(plot_data$lower_ci)
+  global_x_max <- max(plot_data$upper_ci)
+
+  # Add padding for annotations
+  annotation_padding <- (global_x_max - global_x_min) * 0.5
+  global_x_max <- global_x_max + annotation_padding
+  x_loc <- global_x_max - annotation_padding / 2
     
-    ggplot2::ggplot(data = plot_data, aes(x = pt_est, y = subgroup, color = subgroup)) +
+  plot_results <- lapply(unique(plot_data$study_label), function(x, plot_data){
+    sub_data <- plot_data[plot_data$study_label == x,]
+    
+    ggplot2::ggplot(data = sub_data, aes(x = pt_est, y = subgroup, color = subgroup)) +
       geom_point(size = 5) +
       geom_errorbarh(aes(xmin = lower_ci, xmax = upper_ci), height = 0.3) +
       geom_vline(xintercept = 0, linetype = "dashed", color = "red") +
-      geom_text(aes(x = x_loc, 
-                    label = paste0("Point estimate: ", round(pt_est, 4), 
-                                   "\n95% CI: [", round(lower_ci, 4), ", ", round(upper_ci, 4), "]")), 
+      geom_text(aes(x = x_loc,
+                    label = paste0("Point estimate: ", round(pt_est, 4),
+                                   "\n95% CI: [", round(lower_ci, 4), ", ", round(upper_ci, 4), "]")),
                 hjust = 0, vjust = 0.5, size = 5) +
-      labs(title = plot_label,
-           x = "Estimated effect", y = "Treatment subgroup") +
+      labs(title = sub_data$study_label[1],
+           subtitle = sub_data$plot_label[1],
+           x = "Estimated effect",
+           y = "Treatment subgroup") +
       theme_minimal(base_size = 14) +
       labs(color = "Treatment subgroup") +
       theme(
@@ -946,11 +994,12 @@ plot_cis_subplots <- function(results,
         axis.text = element_text(size = 12)
       ) +
       coord_cartesian(xlim = c(global_x_min, global_x_max))
-  })
+    
+  }, plot_data = plot_data)
   
   # Combine plots with patchwork, keeping individual plot titles and shared x-axis
   combined_plot <- patchwork::wrap_plots(plot_results, ncol = 1) +
-    plot_layout(guides = "collect") & 
+    patchwork::plot_layout(guides = "collect") & 
     theme(legend.position = "bottom")
   
   return(combined_plot)
